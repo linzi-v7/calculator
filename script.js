999d7cf8a52a4a90938107c8818739197b3279d6
linzi-v7 <farisos7777@gmail.com>
2024-11-18 16:24:38 +0200

    Adjust logic for calculation and added a simple style to the site

--- script.js
+++ script.js
@@ -1,47 +1,126 @@
 
 let firstNumber = 0;
+let firstNumberChosen = false;
 let chosenOperation = " "
 let secondNumber = 0;
+let showResult = false;
+
 
 const equalsButton = document.querySelector(".equal");
-equalsButton.addEventListener("click", function () { operate(1, 1, "+") })
+equalsButton.addEventListener("click", function ()
+{
+    showResult = true;
+
+    operate(firstNumber, secondNumber, chosenOperation);
+
+    showResult = false;
+})
+
+const digitsContainer = document.querySelector(".calculator-buttons");
+digitsContainer.addEventListener("click", function (event)
+{
+    if (event.target.matches(".digit"))
+    {
+        let input = event.target.innerText;
+        populateDisplay(input);
+    }
+    else if (event.target.matches(".operator"))
+    {
+        //edge case:  calculator should not evaluate more than a single pair of numbers at a time
+        if (chosenOperation != " " && firstNumberChosen)  
+        {
+            showResult = true;
+            operate(firstNumber, secondNumber, chosenOperation);
+            showResult = false;
+            return;
+        }
+        chosenOperation = event.target.innerText;
+        firstNumberChosen = true;
+    }
+    else if (event.target.matches(".clear"))
+    {
+        clearAll();
+    }
+
+});
 
 function operate(num1, num2, operation)
 {
-    if ((num1 == undefined) || (num2 == undefined))
+    if (operation == " ")
     {
-        console.log("error, invalid numbers");
         return;
     }
 
-    console.log("here");
+    let result = 0;
 
     switch (operation)
     {
         case '+':
-            return num1 + num2;
+            result = num1 + num2;
             break;
         case '-':
-            return num1 - num2;
+            result = num1 - num2;
             break;
         case '*':
-            return num1 * num2;
+            result = num1 * num2;
             break;
         case '/':
-            return num1 / num2;
+            if (num2 == 0)
+            {
+                populateDisplay("seriously?");
+                chosenOperation = " ";
+                return;
+            }
+            else
+            {
+                result = num1 / num2;
+            }
             break;
-        default:
-            console.log("error, invalid operation");
+
     }
+
+    if (Number.isInteger(result))
+    {
+        populateDisplay(result);
+    }
+    else
+    {
+        populateDisplay(result.toFixed(2));
+    }
+    firstNumber = result;
+    firstNumberChosen = true;
+    chosenOperation = " ";
+
 }
 
 function populateDisplay(input)
 {
+
     let display = document.querySelector(".calculator-display");
 
-    if (display.innerText == "0")
+    if (display.innerText == "0" || showResult == true || firstNumberChosen == true)
     {
         display.innerText = "";
     }
-    display.innerText += input
+    display.innerText += input;
+
+    if (chosenOperation != " ") //if user chose operation means he chose the first number
+    {
+        secondNumber = Number(display.innerText);
+
+    } else
+    {
+        firstNumber = Number(display.innerText);
+    }
+}
+
+function clearAll()
+{
+    populateDisplay(0)
+    firstNumber = 0;
+    firstNumberChosen = false;
+    secondNumber = 0;
+    chosenOperation = " ";
+    showResult = false;
+
 }
